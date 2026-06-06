e8eaa9039743ca808e21fb9e9b9086dcd2545513
reyjag <reyjaguines@gmail.com>
2026-06-06 18:28:47 +0300

    adding browser tab icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Jaguines Digital Solutions')</title>
+    <link rel="icon" type="image/png" href="/images/logo.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
